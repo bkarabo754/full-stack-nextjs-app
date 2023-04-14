daa00e8cf632b81e4049f752af7724becadf3f8c
Bongani Masango <bkmasango26@gmail.com>
2023-04-14 21:52:16 +0200

    data

--- pages/products.js
+++ pages/products.js
@@ -1,8 +1,11 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 
 export default function Products() {
     return (
-        <Layout>products page here</Layout>
+        <Layout>
+            <Link className="bg-[#0F1624] text-white rounded-md py-1 px-2" href={"/products/new"}>Add new product</Link>
+        </Layout>
     )
 }
